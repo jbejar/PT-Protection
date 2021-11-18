07d7607dd2ffe82d551787cfc6d4cb3b67f303b0
Jonathan Bejarano <jonathan.bejarano@coxautoinc.com>
2021-11-18 06:42:32 -0700

    decreasing the quality

--- assets/img/gallery/re.sh
+++ assets/img/gallery/re.sh
@@ -3,6 +3,6 @@ echo "Width: $2"
 ext="${1#*.}"
 file="${1%%.*}"
 mv $1 $file-full.$ext
-convert $file-full.$ext -resize $2x $1
+convert -strip -interlace Plane -gaussian-blur 0.05 -quality 85% $file-full.$ext -resize $2x $1
 wc -c $file-full.$ext
 wc -c $1
